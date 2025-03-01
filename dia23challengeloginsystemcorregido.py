9c2c52c7ed7bd3ce6f750ba9cd53586f73a0c67d
Jesús Aular <aularje@yahoo.com>
2025-03-01 19:59:10 -0400

    Add files via upload

---
+++ dia23challengeloginsystemcorregido.py
@@ -0,0 +1,23 @@
+def login():
+    #usuario ingresa su nombre y contraseña
+    nombre = input("Ingrese su nombre:")
+    contraseña = input("Ingrese su contraseña:")
+    #base de datos de claves
+    usuario = "jesus"
+    clave = "tres3palos"
+    #comparación
+    if nombre == usuario and contraseña == clave:
+        print("BIENVENIDO")
+        k = 1
+    else:
+        print("no reconozco el nombre o contraseña, vuelva a intentar")
+        k = 0
+    return(k)
+
+while True:
+    l = login()
+    if l == 1:
+        break
+    else:
+        continue
+    
